4aab8d6b1d1502020ceacfe31ccb69920b149723
AshishVGajbhiye <ashish.gajbhiye@gmail.com>
2024-01-01 10:43:39 +0530

    Package created

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Document Chatbot',
+    version= '0.0.0',
+    author= 'Ashish Gajbhye',
+    author_email= 'ashish.gajbhiye@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
